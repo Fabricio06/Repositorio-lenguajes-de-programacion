6b5604f87ee634c93f46b162979ab9340cfbb806
Fabricio Porras Morera <faporras05@hotmail.com>
2023-02-22 22:39:04 -0600

    Ejercicio_1 completed 100%

--- Paradigma Imperativo/ejercicio_1.go
+++ Paradigma Imperativo/ejercicio_1.go
@@ -1,5 +1,5 @@
 /*
-		Solucion del ejercicio 1 a realizar
+					Solucion del ejercicio 1 a realizar
 	 1. Haga un programa que cuente e indique el número de caracteres,
 	    el número de palabras y el número de líneas de un texto cualquiera
 	    (obtenido de cualquier forma que considere).
@@ -9,54 +9,37 @@ package main
 
 import "fmt"
 
-func contador(texto string) {
-	var numero_caracteres int
-	var numero_palabras int
-	var numero_lineas int
-
-	//En el caso de que se mande solo un caracter, no cuenta como palabra
-	if len(texto) <= 1 {
-		numero_caracteres = len(texto)
-		numero_palabras = 0
-		numero_lineas = 1
-
-	} else {
-		var separador bool = false
-		numero_palabras++
-		for x := 0; x < len(texto); x++ {
-
-			if separador == true {
-				separador = false
-				numero_palabras++
-				numero_caracteres++
-			} else if string(texto[x]) != " " {
-				numero_caracteres++
-			} else if string(texto[x]) == " " {
-				separador = true
-			}
-
-		}
-	}
-
-	fmt.Println("El texto: '"+texto+"'\n"+
-		"analizando...\n"+
-		"numero de caracteres: ", numero_caracteres, "\n"+
-		"numero de palabras: ", numero_palabras, "\n"+
-		"numero de lineas: ", numero_lineas)
-
+func main() {
+	contador("A statement can precede conditionals any variables declared in this statement are available in the current and all subsequent branches")
+	contador("Hola, mi nombre es Fabricio\n me gusta programar\n entre otras cosas")
 }
 
-func contador2(texto string) {
+/*
+La solucion que implemente, se encarga de tomar el len del texto y se le restan
+los espacios, valiendo como los caracteres totales que existen
+
+# No se toman en cuenta si el usuario dijita mas de un espacio entre palabras
+
+# Las vocales se toman como palabras al igual que word
+
+# A los caracteres se le restan el valor de \n palabras encontradas
+*/
+func contador(texto string) {
+
+	fmt.Println()
 	var espacios_blancos int
+	var cambios int
 
 	for x := 0; x < len(texto); x++ {
 		if string(texto[x]) == " " {
 			espacios_blancos++
+		} else if string(texto[x]) == "\n" {
+			cambios++
 		}
 	}
 
-	var numero_caracteres int = len(texto) - espacios_blancos
-	var numero_palabras int = espacios_blancos
+	var numero_caracteres int = len(texto) - (espacios_blancos + cambios)
+	var numero_palabras int = espacios_blancos + 1 //Por la primera palabra
 	var numero_lineas int
 
 	fmt.Println("El texto: '"+texto+"'\n"+
@@ -64,9 +47,5 @@ func contador2(texto string) {
 		"numero de caracteres: ", numero_caracteres, "\n"+
 		"numero de palabras: ", numero_palabras, "\n"+
 		"numero de lineas: ", numero_lineas)
-}
-
-func main() {
-	//contador("A statement can precede conditionals any variables declared in this statement are available in the current and all subsequent branches")
-	contador2("A statement can precede conditionals any variables declared in this statement are available in the current and all subsequent branches")
+	fmt.Println()
 }
