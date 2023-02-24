868827b3e5e5d736b49b6c507d0f6b37df6079c6
Fabricio Porras Morera <faporras05@hotmail.com>
2023-02-24 00:01:42 -0600

    Completed 100% ejercicio_5

--- Paradigma Imperativo/ejercicio_5.go
+++ Paradigma Imperativo/ejercicio_5.go
@@ -10,7 +10,7 @@ Como ejemplos de expresiones y de evaluación se adjuntan las siguientes figuras
 en las expresiones en infijo y en el algoritmo de evaluación usando pilas.
 			---Evaluacion de izquierda a derecha--->
 
-	4		5		6		*				+
+//	4		5		6		*				+
 	|		|		|		|				|
     |		|		|	 extraer dos	extraer dos
 	|		|		|	 veces y hacer	veces y hacer
@@ -20,12 +20,51 @@ en las expresiones en infijo y en el algoritmo de evaluación usando pilas.
    [4]     [4]     [4] 	   [4]			   [34]
 */
 
+type pila []int
 
+var miPila pila
 
-Implemente un programa que, utilizando pilas creadas a base de slices, realice un evaluador de expresiones
-matemáticas expresadas en notación inflija. Se debe probar el programa a partir de al menos 3 expresiones con diferentes
-características dadas por el programador (el main tendrá tres llamadas al evaluador con 3 expresiones diferentes)
+func (p *pila) agregar(n int) {
+	*p = append(*p, n)
+}
+func (p *pila) sacar() (n int) {
+	valor := (*p)[len(*p)-1]
+	*p = append((*p)[:len(*p)-1])
+	return valor
+}
 
-func main(){
-	fmt.Println()
+func (p *pila) evaluadorExp(exprex string) {
+	if len(*p) < 2 {
+		fmt.Println("Necesita al menos 2 elementos en la pila")
+	} else {
+		var resultado int
+		switch exprex {
+		case "*":
+			valor1 := (*p).sacar()
+			valor2 := (*p).sacar()
+			resultado = valor1 * valor2
+		case "+":
+			valor1 := (*p).sacar()
+			valor2 := (*p).sacar()
+			resultado = valor1 + valor2
+		case "-":
+			valor1 := (*p).sacar()
+			valor2 := (*p).sacar()
+			resultado = valor1 - valor2
+		}
+		(*p).agregar(resultado)
+	}
+}
+
+func main() {
+	fmt.Println(miPila)
+	miPila.agregar(4)
+	miPila.agregar(5)
+	miPila.agregar(6)
+	fmt.Println(miPila)
+	miPila.evaluadorExp("*")
+	fmt.Println(miPila)
+	miPila.evaluadorExp("+")
+	fmt.Println(miPila)
+
 }
